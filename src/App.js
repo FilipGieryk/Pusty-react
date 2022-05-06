ce31ee5ff35c01563f6cb123f9a9fb54403cebdd
Filip <filipgcs@gmail.com>
2022-05-06 14:57:52 +0200

    create project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Pusty projekt</h1>
+    <p>&#x1F632;</p>
+    </div>
+  );
+}
+
+export default App;
